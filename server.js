1c7fbf616bb14847d2937289553da93539a76ef3
paolocasison <paolocasison@gmail.com>
2024-04-06 23:59:53 -0400

    fixed backend

--- server.js
+++ server.js
@@ -1,43 +1,57 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const { ApolloServer } = require('apollo-server-express');
-const app = express();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+mongoose.set("strictQuery", false);
+
+//import typedefs and resolvers
+const TypeDefs = require("./schema");
+const Resolvers = require("./resolvers");
+
+//import ApolloServer
+const { ApolloServer } = require("apollo-server-express");
+
+//Store sensitive information to env variables
+const dotenv = require("dotenv");
+dotenv.config();
+
+//mongoDB Atlas Connection String
+const mongodb_atlas_url = process.env.MONGO_URL;
+
 //TODO - Replace you Connection String here
-const DB_USER = 'paolocasison';
-const DB_USER_PASSWORD = 'Password.123';
-const DB_CLUSTER = 'cluster0.3myz13n.mongodb.net';
-const DB_NAME = 'comp3133_assignment1';
-const mongodb_atlas_url = `mongodb+srv://${DB_USER}:${DB_USER_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?retryWrites=true&w=majority`;
-mongoose.connect(mongodb_atlas_url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(success => {
-    console.log('Success Mongodb connection')
-  }).catch(err => {
-    console.log('Error Mongodb connection')
-  });
+mongoose
+    .connect(mongodb_atlas_url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then((success) => {
+        console.log("Success Mongodb connection");
+    })
+    .catch((err) => {
+        console.log("Error Mongodb connection", err);
+    });
 
-  // Apollo GraphQL Setup
-const userResolvers = require('./graphql/userResolvers');
-const userTypeDefs = require('./graphql/userTypeDefs');
-const empResolvers = require('./graphql/empResolvers');
-const empTypeDefs = require('./graphql/empTypeDefs');
+//Define Express Server
+const app = express();
+app.use(bodyParser.json());
+app.use("*", cors());
 
-const typeDefs = [userTypeDefs, empTypeDefs];
-const resolvers = [userResolvers, empResolvers];
+// Defining Apollo Server
 
-async function startServer(typeDefs, resolvers) {
-    const server = new ApolloServer({ 
-        typeDefs, 
-        resolvers,
-        graphiql: true
+let server = null;
+async function startServer() {
+    server = new ApolloServer({
+        typeDefs: TypeDefs.typeDefs,
+        resolvers: Resolvers.resolvers,
     });
     await server.start();
-
-    server.applyMiddleware({ app, path: '/graphql'});
-    app.listen({ port: 4000 }, () =>
-        console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-    );
+    server.applyMiddleware({ app });
 }
+startServer();
 
-startServer(typeDefs, resolvers);
+//Start listen
+app.listen({ port: process.env.PORT }, () =>
+    console.log(
+        `🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`
+    )
+);
